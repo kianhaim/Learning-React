438348df06a3aa8d257945efe0ca7648db7f7b85
kianhaim <kristianhaim@gmail.com>
2021-09-18 14:09:38 -0500

    Commit

---
+++ Section 15/src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = props => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = props => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
