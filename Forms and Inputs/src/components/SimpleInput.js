9f40c8a359a0ede83e0de40ef3444763e734b0fe
kianhaim <kristianhaim@gmail.com>
2021-09-19 12:03:43 -0500

    ref

--- Forms and Inputs/src/components/SimpleInput.js
+++ Forms and Inputs/src/components/SimpleInput.js
@@ -1,11 +1,39 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from "react";
+
+const SimpleInput = props => {
+  const nameInputRef = useRef();
+
+  // Validated every key stroke , and can be reset
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    setEnteredName("");
+
+    // Use for one once validation eg. submitting the form
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          // ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
