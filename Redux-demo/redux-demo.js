12abcb123806c411ffbc1bd1738f8c05e364f01e
kianhaim <kristianhaim@gmail.com>
2021-09-20 14:20:28 -0500

    Intro to Redux

---
+++ Redux-demo/redux-demo.js
@@ -0,0 +1,23 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  return state;
+};
+const store = redux.createStore(counterReducer);
+
+//console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
