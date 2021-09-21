656128d0b92275259315026a8bbaad73ca8ec094
kianhaim <kristianhaim@gmail.com>
2021-09-21 16:34:26 -0500

    Fetch Data

--- Advance Redux/src/store/cart-slice.js
+++ Advance Redux/src/store/cart-slice.js
@@ -10,11 +10,12 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
-          quanty: 1,
+          quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title,
         });
@@ -26,8 +27,9 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.item = state.items.filter(item => item.id !== id);
+        state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
