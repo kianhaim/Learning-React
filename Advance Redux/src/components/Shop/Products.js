5a09dd7a66556081137177cad54766721232b766
kianhaim <kristianhaim@gmail.com>
2021-09-21 13:54:28 -0500

    Redux Add Cart

--- Advance Redux/src/components/Shop/Products.js
+++ Advance Redux/src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
-const Products = (props) => {
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "The first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 12,
+    title: "My Second Book",
+    description: "The Second book I ever wrote",
+  },
+];
+
+const Products = props => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
