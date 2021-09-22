cff3ea3b9aa5e1f14111058a8bf6f06f8327cff9
kianhaim <kristianhaim@gmail.com>
2021-09-21 20:39:28 -0500

    Final

--- Advance Redux/src/App.js
+++ Advance Redux/src/App.js
@@ -3,63 +3,29 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
-let isInitail = true;
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
-
-  //Listening to the cart
   const cart = useSelector(state => state.cart);
-
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "Pending",
-          title: "Sending",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://customhooks-f5a29-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sending cart data successfully!",
-        })
-      );
-    };
-
-    if (isInitail) {
-      isInitail = false;
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
       return;
     }
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
