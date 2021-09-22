b86b8e540465033dde6befc461cddf2de497c9c4
kianhaim <kristianhaim@gmail.com>
2021-09-22 10:56:44 -0500

    Router Links

--- Multi-Pag SPA with React Rounting/src/App.js
+++ Multi-Pag SPA with React Rounting/src/App.js
@@ -1,9 +1,25 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+//our-domain.com/welcome => Welcome Component
+//our-domain.com/products => Products Component
---
+++ Multi-Pag SPA with React Rounting/src/components/MainHeader.js
@@ -0,0 +1,17 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href='/welcome'> Welcome</a>
+          </li>
+          <li>
+            <a href='/products'> Products</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainHeader;
