a2bbaf56d029a7725ca2d17d33eea4166add820a
kianhaim <kristianhaim@gmail.com>
2021-09-23 09:07:07 -0500

    Final

--- React Routing Project/src/pages/AllQuotes.js
+++ React Routing Project/src/pages/AllQuotes.js
@@ -1,14 +1,38 @@
 import QuoteList from "../components/quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React makes you crazy!" },
-  { id: "q2", author: "Maxi", text: "Learning React is insane!" },
-  { id: "q3", author: "Maxim", text: "Learning React is not fun!" },
-  { id: "q4", author: "Maximil", text: "Learning React will kill you!" },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import { useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0))
+    return <NoQuotesFound />;
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
--- React Routing Project/src/pages/NewQuotes.js
+++ React Routing Project/src/pages/NewQuotes.js
@@ -1,14 +1,25 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuotes = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
-  const addQuoteHandler = quoteData => {
-    console.log(quoteData);
 
-    history.push("/quotes");
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = quoteData => {
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuotes;
