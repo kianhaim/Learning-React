18f961a77ebc2d2f655a2a49ab059949f066eafa
kianhaim <kristianhaim@gmail.com>
2021-09-28 13:10:25 -0500

    Final

--- Replace-redux/src/components/Products/ProductItem.js
+++ Replace-redux/src/components/Products/ProductItem.js
@@ -1,20 +1,24 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+import React from "react";
+import { useStore } from "../../hooks-store/store";
 
 import Card from "../UI/Card";
-import { ProductsContext } from "../../context/products-context";
+//import { ProductsContext } from "../../context/products-context";
 import "./ProductItem.css";
 
 //import { useDispatch } from 'react-redux';
 //import { toggleFav } from "../../store/actions/products";
 
-const ProductItem = props => {
-  const toggleFav = useContext(ProductsContext).toggleFav;
-  //const dispatch = useDispatch();
+const ProductItem = React.memo(props => {
+  console.log("RENDERING..");
 
+  //const toggleFav = useContext(ProductsContext).toggleFav;
+  //const dispatch = useDispatch();
+  const dispatch = useStore(false)[1];
   const toggleFavHandler = () => {
     // dispatch(toggleFav(props.id));
-
-    toggleFav(props.id);
+    // toggleFav(props.id);
+    dispatch("TOGGLE_FAV", props.id);
   };
 
   return (
@@ -31,6 +35,6 @@ const ProductItem = props => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- Replace-redux/src/containers/Products.js
+++ Replace-redux/src/containers/Products.js
@@ -1,15 +1,18 @@
-import React, { useContext } from "react";
+//import React, { useContext } from "react";
+import React from "react";
 
 import ProductItem from "../components/Products/ProductItem";
-import { ProductsContext } from "../context/products-context";
+//import { ProductsContext } from "../context/products-context";
+import { useStore } from "../hooks-store/store";
 import "./Products.css";
 
 const Products = props => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
+  //const productList = useContext(ProductsContext).products;
 
   return (
     <ul className='products-list'>
-      {productList.map(prod => (
+      {state.products.map(prod => (
         <ProductItem
           key={prod.id}
           id={prod.id}
